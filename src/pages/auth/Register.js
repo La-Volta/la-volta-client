f2187411076f7ab5cd00edc3b5a0407e50346111
maricarmen <mchuecooviedo@gmail.com>
2023-03-09 15:03:15 +0100

    css init

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -6,6 +6,7 @@ function Register() {
 
   const [registerInput, setRegister] = useState({
     name: '',
+    firstname:'',
     email: '',
     password: '',
     error_list: [],
@@ -39,31 +40,43 @@ const registerSubmit = (e) => {
   return (
     <div>
 
-      <h6>Fes-te amic de La Volta</h6>
-      <h6>Per a fer efectiva la donació és necessari registrar-se i seguir els passos indicats</h6>
+      <div className="text-center">
+        <h6 class='pt-5'>Fes-te amic de La Volta</h6>
+        <h6 class='pt-5'>Per a fer efectiva la donació és necessari <br>
+        </br>registrar-se i seguir els passos indicats</h6>
+      </div>
+      
 
       <div className="container py-5">
-           <div className="row justify-content-center">
+        <div className="row justify-content-center">
                     <div className="col-md-6">
                         <div className="card">
                             <div className="card-header">
                               
-                                <h4>Register</h4>
+                                <h4 class="text-center">Register</h4>
                             </div>
                             <div className="card-body">
                                 <form onSubmit={registerSubmit}>
                                     <div className="form-group mb-3">
-                                        <label>Full Name</label>
+                                        <label for='name'>Nom</label>
                                         <input type="text" name="name" onChange={handleInput} value={registerInput.name} className="form-control"  />
                                         <span>{registerInput.error_list.name}</span>
                                     </div>
+
+                                    <div className="form-group mb-3">
+                                        <label for='firstname'>Cognoms</label>
+                                        <input type="text" name="fisrtname" onChange={handleInput} value={registerInput.firstname} className="form-control"  />
+                                        <span>{registerInput.error_list.firstname}</span>
+                                    </div>
+
                                     <div className="form-group mb-3">
                                         <label>Email ID</label>
                                         <input type="text" name="email" onChange={handleInput} value={registerInput.email} className="form-control"  />
                                         <span>{registerInput.error_list.email}</span>
                                     </div>
+
                                     <div className="form-group mb-3">
-                                        <label>Password</label>
+                                        <label>Contraseyna</label>
                                         <input type="text" name="password" onChange={handleInput} value={registerInput.password} className="form-control"  />
                                         <span>{registerInput.error_list.password}</span>
                                     </div>
