bb099d7efbeac476668d4a2ce24e548dc706a7a8
bgiudicid <b.giudicid@gmail.com>
2023-03-12 23:40:54 +0100

    logout under construction

--- src/application/Router.js
+++ src/application/Router.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, } from 'react-router-dom';
 import Home from '../pages/Home';
 import Dashboard from '../pages/admin/Dashboard';
 import Profile from '../pages/admin/Profile';
 import Login from '../pages/auth/Login';
 import Register from '../pages/auth/Register';
 
+
 const Router = () => {
+
+
   return (
     <BrowserRouter>
         <Routes>
@@ -14,9 +17,16 @@ const Router = () => {
             <Route path="/" element={<Home/>} />
             <Route path="/admin/dashboard" element={<Dashboard/>} />
             <Route path="/admin/profile" element={<Profile/>} />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/register" element={<Register/>} />
+            {/*<Route path="/login" element={<Login/>} />
+            <Route path="/register" element={<Register/>} /> */}
 
+            <Route path="/login">
+              {localStorage.getItem('auth_token') ? <Navigate to='/' /> : <Login />}
+            </Route>
+
+            <Route path="/register" >
+              {localStorage.getItem('auth_token') ? <Navigate to='/' /> : <Register />}
+            </Route>
         
         </Routes>
     </BrowserRouter>
