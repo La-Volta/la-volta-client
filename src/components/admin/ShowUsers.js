2a27cbf9684e7a164f5ba089b96b0c11690885d5
juliana montano <zjulim89@gmail.com>
2023-03-26 19:26:39 +0200

    fixing admin styles

--- src/components/admin/ShowUsers.js
+++ src/components/admin/ShowUsers.js
@@ -24,14 +24,14 @@ const ShowUsers = () => {
        getAllUsers()
     }
   return (
-    <div className='d-grid gap-2'>
+    <div className='d-grid gap-2 row d-flex justify-content-center'>
                 
                 <div className='text-success text-center'>
                     <h1>Hola Admin!</h1>
                     <h3>Aquest és el teu perfil admin de La Volta</h3>
                 </div>
                 <table className='table table-striped'>
-                    <thead className='bg-primary text-white'>
+                    <thead className='bg-black text-white'>
                         <tr>
                             <th>Id</th>
                             <th>Name</th>
@@ -51,9 +51,9 @@ const ShowUsers = () => {
                                 <td className='text-success'> {user.email} </td>
                                 <td className='text-success'> {user.pyments} </td>
                                 <td>
-                                    <Link to="/register" className='btn btn-success btn-lg mt-2 mb-2 text-white'>Create</Link>
-                                    <Link to={`/edit/${user.id}`} className='btn btn-warning'>Edit</Link>
-                                    <button onClick={ ()=>deleteUser(user.id) } className='byn btn-danger'>Delete</button>
+                                    <Link to="/register" className='btn btn-danger btn-sb mt-2 mb-1  text-white'>Create</Link>
+                                    <Link to={`/edit/${user.id}`} className='btn btn-danger mx-2 mb-1'>Edit</Link>
+                                    <button onClick={ ()=>deleteUser(user.id) } className='btn btn-danger'>Delete</button>
                                 </td>
                             </tr>
                         )) }
