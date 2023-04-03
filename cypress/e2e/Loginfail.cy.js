db88155676482ec05445b73f85ea1cea4e012e49
bgiudicid <b.giudicid@gmail.com>
2023-04-03 13:28:10 +0200

    tests

---
+++ cypress/e2e/Loginfail.cy.js
@@ -0,0 +1,36 @@
+/// <reference types="Cypress" />
+
+
+      
+     
+      cy.get('.mt-4 > :nth-child(3)').should('contain','¡Usuario y/o Contraseña incorrectos!');
+      cy.get('.mt-4 > :nth-child(4)').should('contain','Por favor acepta el captcha.');
+    });
+  });
+
+  describe ('fail login', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/')
+      });
+
+    
+    it('failed to login', function () {
+    let email = 'prueba@gmail.com'
+    let password = '01234567'
+
+    cy.visit('http://localhost:3000/login')
+
+    cy.get('.navbar-brand')
+    cy.wait(500)
+    cy.get('input[name=email]').click({ force: true }).type(email)
+    cy.wait(500)
+    cy.get("input[name=password]").type(password);
+    cy.wait(500);
+    cy.get('.swal2-modal').click()
+    cy.wait(1000)
+    cy.get('.swal2-modal').should('contain','¡Usuario y/o Contraseña incorrectos!');
+    
+    
+    
+
+    })
