0f1928e077d0ef6bc90608ef1f06c727608b9c96
bgiudicid <b.giudicid@gmail.com>
2023-04-04 10:04:30 +0200

    tests done

--- cypress/e2e/FailedLogin.cy.js
+++ cypress/e2e/FailedLogin.cy.js
@@ -2,24 +2,18 @@ describe ('Fail login', () => {
   beforeEach(() => {
       cy.visit('http://localhost:3000/')
     })
-
-    
     it('failed to login', function () {
     let email = 'prueba@gmail.com'
     let password = '01234567'
-
     cy.visit('http://localhost:3000/login')
-
     cy.get('.navbar-brand')
-    cy.wait(500)
-    cy.get('input[name=email]').click({ force: true }).type(email)
-    cy.wait(500)
-    cy.get("input[name=password]").type(password);
-    cy.wait(500);
-    cy.get('.swal2-modal').click()
     cy.wait(1000)
-    cy.get('.swal2-modal').should('contain','¡Usuario y/o Contraseña incorrectos!')
-
+    cy.get('input[name=email]').click({ force: true }).type(email)
+    cy.wait(1000)
+    cy.get("input[name=password]").click({ force: true }).type(`${password}{enter}`);
+    cy.wait(4000);
+    cy.get('.swal2-modal').contains("error")
+    cy.wait(1000)
+    cy.url().should('include', 'http://localhost:3000/login')
   })
-
 })
